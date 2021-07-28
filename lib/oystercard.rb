716a1396a1025822770020f004ef58f549538a2d
danyaal <danyaalibnasif@gmail.com>
2021-07-28 16:59:18 +0100

    finished ch13 - created station class

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,16 @@
 class Oystercard
   
-  attr_accessor :balance 
+  attr_accessor :balance, :entry_station, :exit_station, :journeys
   MAXIMUM_BALANCE = 50
   MINIMUM_BALANCE = 1
 
   def initialize
     @balance = 0 
+    @entry_station = nil
+    @exit_station = nil
+    @journeys = []
+    @journey = {}
+
   end
     
   def top_up(amount)
@@ -14,21 +19,28 @@ class Oystercard
     @balance += @upper_amount
   end
 
-  def deduct(amount)
-    @balance -= amount
-  end
 
-  def touch_in
+  def touch_in(entry_station)
     raise "you need a minimum of $#{MINIMUM_BALANCE} balance for a single journey" if minimum?
-    @in_journey = true
+    @entry_station = entry_station
   end
 
-  def touch_out
-    @in_journey = false
+  def touch_out(exit_station)
+
+    deduct(MINIMUM_BALANCE)
+    
+
+    @journey = {:entry_station => entry_station, :exit_station => exit_station}
+    @journeys << @journey
+    @exit_station = exit_station
+
+    @entry_station = nil
+
   end
 
   def in_journey?
-    @in_journey 
+    !!entry_station 
+    # in_journey will return true unless the value of entry_station is false or nil.
   end
 
 
@@ -40,6 +52,11 @@ class Oystercard
   def minimum?
     @balance < MINIMUM_BALANCE
   end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
 
 
     
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,11 @@
-require "Oystercard"
+require_relative "../lib/oystercard.rb"
 
 describe Oystercard do
+
+  let(:entry_station) {double :station}
+
+  let(:exit_station) {double :station}
+  
   
   it "has a balance" do
     expect(subject.balance).to eq(0)
@@ -17,25 +22,64 @@ describe Oystercard do
     expect{subject.top_up(1)}.to raise_error("maximum balanced of $#{maximum_balance} has been exceeded")
   end
 
-  it "deducts money from the card" do
-    subject.top_up(20)
-    expect{subject.deduct(10)}.to change {subject.balance}.by -10
-  end
+  #private method now
+
+  #it "deducts money from the card" do
+   # subject.top_up(20)
+    #expect{subject.deduct(10)}.to change {subject.balance}.by -10
+  #end
 
   it "registers touch in" do
     subject.top_up(10)
-    subject.touch_in
+    subject.touch_in(entry_station)
     expect(subject.in_journey?).to eq true
     
   end
 
   it "registers touch out" do
-    subject.touch_out
+    subject.touch_out(exit_station)
     expect(subject.in_journey?).to eq false
   end
 
   it "has a minimum of $1 balance to touch in" do
     minimum_balance = Oystercard::MINIMUM_BALANCE
-    expect{subject.touch_in}.to raise_error("you need a minimum of $#{minimum_balance} balance for a single journey")
+    expect{subject.touch_in(entry_station)}.to raise_error("you need a minimum of $#{minimum_balance} balance for a single journey")
   end
+
+  it "deducts correct amount from the card when the journey is complete" do
+    subject.top_up(3)
+    subject.touch_in(entry_station)
+    expect{subject.touch_out(exit_station)}.to change{subject.balance}.by -Oystercard::MINIMUM_BALANCE
+   end
+
+
+  it "saves the entry stations, where you've travelled from" do
+    subject.top_up(3)
+    subject.touch_in(entry_station) 
+    expect(subject.entry_station).to eq entry_station
+
+  end
+
+  it "saves the exit station" do
+    subject.top_up(3)
+    subject.touch_out(exit_station)
+    expect(subject.exit_station).to eq exit_station
+  end
+
+
+  let(:journey){ [{entry_station: entry_station, exit_station: exit_station}] }
+  
+  it "stores the entry and exit stations in a hash" do
+    subject.top_up(3)
+    subject.touch_in(entry_station)
+    subject.touch_out(exit_station)
+    expect(subject.journeys).to eq journey
+  end
+
+  it "has an empty list of journeys by default" do
+    expect(subject.journeys).to eq [] 
+  end
+
 end
+
+
